433ff94ba416d377af0c0df69a25cbdd363ac4c3
Nathan Daly <nhdaly@gmail.com>
2018-02-28 01:00:05 -0600

    Make it a proper command-line switched program.
    Add using ArgParse to allow specifying all args on commandline, rather
    than having to edit the script directly.

--- build.jl
+++ build.jl
@@ -1,15 +1,67 @@
-using Glob
+using Glob, ArgParse
 
-APPNAME="PowerPong"
+s = ArgParseSettings()
 
-jl_main_file = "pongmain.jl"
+@add_arg_table s begin
+    "juliaprog_main"
+        arg_type = String
+        required = true
+        help = "Julia program to compile -- must define julia_main()"
+    "appname"
+        arg_type = String
+        default = nothing
+        help = "name to call the generated .app bundle"
+    "builddir"
+        arg_type = String
+        default = "builddir"
+        help = "directory used for building, either absolute or relative to the Julia program directory"
+    "--verbose", "-v"
+        action = :store_true
+        help = "increase verbosity"
+    "--resource", "-R"
+        arg_type = String
+        action = :append_arg  # Can specify multiple
+        default = nothing
+        metavar = "<resource>"
+        help = """specify files or directories to be copied to
+                  MyApp.app/Contents/Resources/. This should be done for all
+                  resources that your app will need to have available at
+                  runtime. Can be repeated."""
+    "--lib", "-L"
+        arg_type = String
+        action = :append_arg  # Can specify multiple
+        default = nothing
+        metavar = "<file>"
+        help = """specify user library files to be copied to
+                  MyApp.app/Contents/Libraries/. This should be done for all
+                  libraries that your app will need to reference at
+                  runtime. Can be repeated."""
+    "--icns"
+        arg_type = String
+        default = nothing
+        metavar = "<file>"
+        help = ".icns file to be used as the app's icon"
+end
+s.epilog = """
+    examples:\n
+    # Build HelloApp.app from hello.jl\n
+    \ua0\ua0 build.jl hello.jl HelloApp\n
+    # Build MyGame, and copy in imgs/, mus.wav and all files in libs/\n
+    \ua0\ua0 build.jl -R imgs -R mus.wav -L lib/* main.jl MyGame
+    """
+
+parsed_args = parse_args(ARGS, s)
+
+APPNAME=parsed_args["appname"]
+
+jl_main_file = parsed_args["juliaprog_main"]
 binary_name = match(r"([^/.]+)\.jl$", jl_main_file).captures[1]
 bundle_identifier = lowercase("com.$(ENV["USER"]).$APPNAME")
 bundle_version = "0.1"
-#icns_file = nothing
-icns_file = "icns.icns"
-user_assets_dir = "assets"    # Contents will be copied to Resources/assets/
-user_libs_dir = "libs"        # Contents will be copied to Libraries/
+icns_file = parsed_args["icns"]
+user_resources = parsed_args["resource"]
+user_libs = parsed_args["lib"]        # Contents will be copied to Libraries/
+verbose = parsed_args["verbose"]
 
 # ----------- Initialize App ---------------------
 println("~~~~~~ Creating mac app in $(pwd())/builddir/$APPNAME.app ~~~~~~~")
@@ -25,12 +77,20 @@ mkpath(resourcesDir)
 mkpath(libsDir)
 
 # ----------- Copy user libs & assets -------------
-println("~~~~~~ Copying '$user_libs_dir' & '$user_assets_dir' to bundle... ~~~~~~~")
-
+println("~~~~~~ Copying user-specified libraries & resources to bundle... ~~~~~~~")
 # Copy assets and libs early so they're available during compilation.
-#  This is mostly relevant if you have .dylibs in your assets, which the compiler wants to look at.
-isdir(user_assets_dir) && run(`cp -rf $user_assets_dir $resourcesDir/`) # note this copies the entire *dir* to Resources
-isdir(user_libs_dir) && run(`cp -rf $(glob("*",user_libs_dir)) $libsDir/`) # note this copies the entire *dir* to Resources
+#  This is mostly relevant if you have .dylibs in your assets, which the compiler needs to look at.
+
+run_verbose(verbose, cmd) = (verbose && println("    julia> run($cmd)") ; run(cmd))
+
+for res in user_resources
+    println("  .. $res ..")
+    run_verbose(verbose, `cp -rf $(glob(res)) $resourcesDir/`) # note this copies the entire *dir* to Resources
+end
+for lib in user_libs
+    println("  .. $lib ..")
+    run_verbose(verbose, `cp -rf $(glob(lib)) $libsDir/`) # note this copies the entire *dir* to Resources
+end
 
 # ----------- Copy julia system libs ---------------------
 println("~~~~~~ Copying julia system libs to bundle... ~~~~~~~")
@@ -125,7 +185,8 @@ cp(icns_file, "$resourcesDir/$APPNAME.icns", remove_destination=true);
 println("~~~~~~ Cleaning up temporary files... ~~~~~~~")
 
 # Delete the tmp build files
-rm("$launcherDir/tmp_v$(string(Base.VERSION))", recursive=true)
+run(`rm -r $(glob("*.ji",launcherDir))`)
+run(`rm -r $(glob("*.o",launcherDir))`)
 
 # Remove debug .dylib libraries and precompiled .ji's
 run(`rm -r $(glob("*.dSYM",libsDir))`)
