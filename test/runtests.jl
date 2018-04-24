f2009c8e8776b8e030a0a4d8a6449348404a6993
Nathan Daly <nhdaly@gmail.com>
2018-04-23 23:35:04 -0500

    Added tests to build the examples/*.jl scripts.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,36 +1,8 @@
-using ApplicationBuilder
 using Base.Test
 
-@testset "change_dir_if_bundle" begin
-global PROGRAM_FILE
-global ENV
-
-@testset "Don't change dir if not compiled." begin
-global PROGRAM_FILE
-PROGRAM_FILE = ""  # PROGRAM_FILE isn't in an app.
-ENV["COMPILING_APPLE_BUNDLE"] = "false"  # Code isn't being compiled for an app.
-reload("ApplicationBuilder")  # redefine function after changing ENV
-cwd = pwd()
-@test cwd == ApplicationBuilder.change_dir_if_bundle()
-@test cwd == pwd()  # didn't change
+@testset "ApplicationBuilder tests" begin
+    Base.invokelatest(include, "ApplicationBuilder.jl")
 end
-
-@testset "*Do* change dir if compiled as app." begin
-# set up fake .App
-tmpdir = mktempdir()
-tmpAppResources = joinpath(tmpdir, "Tmp.app/Contents/Resources")
-tmpAppMacOS = joinpath(tmpdir, "Tmp.app/Contents/MacOS")
-tmpAppExe = joinpath(tmpdir, "Tmp.app/Contents/MacOS/tmp")
-mkpath(tmpAppMacOS)
-mkpath(tmpAppResources)
-
-# Test
-eval(Base, :(PROGRAM_FILE = $tmpAppExe))
-ENV["COMPILING_APPLE_BUNDLE"] = "true"  # Code *is* being compiled
-reload("ApplicationBuilder")  # redefine function after changing ENV
-cwd = pwd()
-# Verify we changed directory
-@test cwd != ApplicationBuilder.change_dir_if_bundle()
-@test Base.Filesystem.samefile(tmpAppResources, pwd())
-end
+@testset "Compiling examples/*.jl" begin
+    include("examples.jl")
 end
