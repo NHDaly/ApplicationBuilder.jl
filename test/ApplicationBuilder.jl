98f41d59fa875528cd00e5be44f261b0e54eb7ef
Nathan Daly <nhdaly@gmail.com>
2018-07-13 14:25:52 -0400

    FINALLY fixed unit tests for change_dir_if_bundle!
    They were `broken` tests, but now they're fixed. :)
    
    Works on both v0.6 and v0.7.

--- test/ApplicationBuilder.jl
+++ test/ApplicationBuilder.jl
@@ -1,18 +1,6 @@
-include(joinpath("..","src","ApplicationBuilder.jl"))  # redefine function after changing ENV
 using Base.Test
 
-@testset "Don't change dir if not compiled." begin
-eval(Base, :(PROGRAM_FILE = ""))
-withenv(()->(include(joinpath("..","src","ApplicationBuilder.jl"))),
-     "COMPILING_APPLE_BUNDLE"=>"false")  # Code is not being compiled.
-sleep(1)  # wait for reload to kick in
-cwd = pwd()
-#  (must use `invokelatest` after `reload` to fix world-age problem.)
-@test cwd == Base.invokelatest(ApplicationBuilder.App.change_dir_if_bundle)
-@test cwd == pwd()  # didn't change
-end
-
-@testset "*Do* change dir if compiled as app." begin
+# Test that change_dir_if_bundle actually changes behavior based on ENV variable
 # set up fake .App
 tmpdir = mktempdir()
 tmpAppResources = joinpath(tmpdir, "Tmp.app/Contents/Resources")
@@ -20,36 +8,24 @@ tmpAppMacOS = joinpath(tmpdir, "Tmp.app/Contents/MacOS")
 tmpAppExe = joinpath(tmpdir, "Tmp.app/Contents/MacOS/tmp")
 mkpath(tmpAppMacOS)
 mkpath(tmpAppResources)
-# Verify we change directory
-eval(Base, :(PROGRAM_FILE = $tmpAppExe))
-withenv(()->(include(joinpath("..","src","ApplicationBuilder.jl"))),
-     "COMPILING_APPLE_BUNDLE"=>"true")  # Code *is* being compiled
-cwd = pwd()
-#  (must use `invokelatest` after `reload` to fix world-age problem.)
-# TODO: except that doesn't work... why doesn't it find the latest function?
-@test_broken cwd != Base.invokelatest(ApplicationBuilder.App.change_dir_if_bundle)
-@test_broken Base.Filesystem.samefile(tmpAppResources, pwd())
 
-# Teardown (reset working directory)
-cd(cwd)
-end
+# Set up test script. (Must be run as a separate process to allow the ENV
+# variable to generate different code at compile-time.)
+testScript = joinpath(tmpdir, "test.jl")
+write(testScript, """
+        # ApplicationBuilder expects to be inside an App structure.
+        eval(Base, :(PROGRAM_FILE = "$tmpAppExe"))
+        # change_dir_if_bundle() will be differently based on the ENV variable.
+        using ApplicationBuilder; ApplicationBuilder.App.change_dir_if_bundle(); println(pwd());
+    """)
 
-# TODO: not sure why the above tests are broken... I tried making a simple toy
-# example, but this one passes... I'm not sure why.
-#
-#module M
-#    foo() = 1
-#end
-#@testset "false" begin
-#withenv(()->(eval(M, :(if get(ENV, "TEST", "false") == "true"; foo() = 2; end))),
-#    "TEST"=>"false")
-#@test 1 == M.foo()
-#@test 1 == Base.invokelatest(M.foo)
-#end
-#@testset "true" begin
-#withenv(()->(eval(M, :(if get(ENV, "TEST", "false") == "true"; foo() = 2; end))),
-#    "TEST"=>"true")
-#@test 1 == M.foo()
-#@test 2 == Base.invokelatest(M.foo)
-#end
-#
+# Without ENV variable set, it should do nothing.
+@test pwd() == readlines(`julia $testScript`)[end]
+# With the ENV variable false, it should do nothing.
+@test pwd() == withenv(()->(readlines(`julia $testScript`)[end]),
+             "COMPILING_APPLE_BUNDLE"=>"false")  # Code is not being compiled.
+
+# With COMPILING_APPLE_BUNDLE == true, the dir should change since it thinks the
+# code is being compiled as an app.
+@test pwd() != withenv(()->(readlines(`julia $testScript`)[end]),
+             "COMPILING_APPLE_BUNDLE"=>"true")  # Code is not being compiled.
