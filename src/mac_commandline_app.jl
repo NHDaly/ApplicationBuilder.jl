82c708235c3974545e15c50a09aae9b8ff681307
Nathan Daly <nhdaly@gmail.com>
2018-07-22 16:02:20 -0400

    Fix commandline_app to create builddir if it doesnt exist already

--- src/mac_commandline_app.jl
+++ src/mac_commandline_app.jl
@@ -10,6 +10,7 @@ end
 
 function build_commandline_app_bundle(builddir, binary_name, appname)
 
+    mkpath(builddir)  # Create builddir if it doesn't already exist.
     applescript_file = "$builddir/build_$appname.applescript"
     exe_dir = "Contents/app"
     write(applescript_file, get_commandline_applescript(exe_dir, binary_name))
