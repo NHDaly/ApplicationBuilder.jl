2ddc651fa2e14c747f39186f4122f613f557baeb
Nathan Daly <nhdaly@gmail.com>
2018-07-30 16:46:53 -0400

    Clean up temp Applescript file in mac_commandline_app

--- src/mac_commandline_app.jl
+++ src/mac_commandline_app.jl
@@ -8,17 +8,22 @@ function get_commandline_applescript(exe_dir, executable_path)
     """
 end
 
-function build_commandline_app_bundle(builddir, binary_name, appname)
+function build_commandline_app_bundle(builddir, binary_name, appname, verbose)
     println("~~~~~~ Creating commandline-app wrapper applet. ~~~~~~~")
 
     mkpath(builddir)  # Create builddir if it doesn't already exist.
     applescript_file = "$builddir/build_$appname.applescript"
     exe_dir = "Contents/app"
+    verbose && println("    Creating Applescript wrapper file: $applescript_file")
     write(applescript_file, get_commandline_applescript(exe_dir, binary_name))
 
     app_path = "$builddir/$appname.app"
     run(`osacompile -o $app_path $applescript_file`)
     mv("$app_path/Contents/MacOS/applet", "$app_path/Contents/MacOS/$binary_name")
+
+    # Cleaning up temporary file:
+    verbose && println("    Cleaning up temporary Applescript file: $applescript_file")
+    rm(applescript_file)
 
     return joinpath(app_path,exe_dir)
 end
