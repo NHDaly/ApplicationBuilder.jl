b71ae670c008014be3fedce148a14d1f5d84c83c
Nathan Daly <nhdaly@gmail.com>
2018-08-09 02:23:18 +0100

    fix 0.7 "\!"

--- examples/commandline_hello.jl
+++ examples/commandline_hello.jl
@@ -4,7 +4,7 @@
 Base.@ccallable function julia_main(ARGS::Vector{String})::Cint
     println("Hi what's your name?")
     name = readline()
-    println("Oh hi, $name\! It's a pleasure to meet you.")
+    println("Oh hi, $name. It's a pleasure to meet you.")
     println("By the way, here's the current working directory:\n'$(pwd())'")
 
     println("\nGoodbye! (Press enter to exit)")
