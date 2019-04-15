18b2222c3db57d19c2ee2c798cf489e0bbe397ec
Nathan Daly <nhdaly@gmail.com>
2019-04-15 19:12:48 -0400

    wrap tests in outer `@testset`

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,13 @@ using Test
 # TODO: Make the tests work on Windows and Linux!!! :'(
 @static if Sys.isapple()
 
+@testset "ApplicationBuilder.jl" begin
 @testset "Test ApplicationBuilder (by compiling examples/*.jl)" begin
     include("ApplicationBuilder.jl")
 end
 @testset "Command-line interface (compiling examples/*.jl)" begin
     include("build_app-cli.jl")
+end
 end
 
 end
